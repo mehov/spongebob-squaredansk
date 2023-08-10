abd6629bad197b7beee2e864409ad64480ae9522
Webmaster <7813306+mehov@users.noreply.github.com>
2023-08-10 12:51:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## SpongeBob SquareDansk
+
+Practice Danish writing using random verbs, conjunctions and situations as input.
+
+## Acknowledgements
+
+- [*Talende verbeliste* a.k.a. *500 MOST COMMON VERBS IN DANISH* by Anders Basby og Søren Kaltoft](http://basby.dk/verbliste/verbeliste1.html)
+- [*Forbinderord (Danish Conjunctions)* by Ewa_Laursen](https://quizlet.com/329507904/forbinderord-danish-conjunctions-flash-cards/)
+- Initial code and SpongeBob suggestions by ChatGPT
+
+## The ChatGPT prompt
+
+> I need a PHP website that displays a random Danish verb, a random Danish conjunction and a random short situation from the "Spongebob Squarepants" TV show in English. Then the website prompts users to write a sentence using all three random values. Upon submission, it will use the OpenAI API to validate whether the entered sentence is grammatically correct in Danish.
+>
+> The verbs and conjunctions to choose from will be stored separately in the best fitting format. I will later edit them to have all the options to choose from.
+>
+> The random Spongebob Situation should also come from asking the OpenAI API. However, have a locally-hosted list as a backup as well.
+>
+> Do not explain. Write only code. Explain in the code comments if needed.
---
+++ index.php
@@ -0,0 +1,127 @@
+<?php
+// Configuration for OpenAI API
+define('OPENAI_API_ENDPOINT', 'https://api.openai.com/v1/chat/completions');
+define('OPENAI_API_KEY', getenv('OPENAI_API_KEY'));
+
+// Read the verbs, conjunctions, and Spongebob situations from separate text files
+$danish_verbs = file('danish_verbs.txt', FILE_IGNORE_NEW_LINES);
+$danish_conjunctions = file('danish_conjunctions.txt', FILE_IGNORE_NEW_LINES);
+$spongebob_situations = file('spongebob_situations.txt', FILE_IGNORE_NEW_LINES);
+
+// Fetch random Danish verb, conjunction, and Spongebob situation
+$random_verb = explode("\t", $danish_verbs[array_rand($danish_verbs)]);
+$random_conjunction = explode("\t", $danish_conjunctions[array_rand($danish_conjunctions)]);
+$random_situation = get_spongebob_situation($spongebob_situations);
+
+function get_spongebob_situation($backup_list) {
+    // Fetch random situation from OpenAI API
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, OPENAI_API_ENDPOINT);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+        'model' => getenv('OPENAI_CHATGPT_MODEL'),
+        'messages' => [
+            [
+                'role' => 'user',
+                'content' => 'Simple situation from Spongebob in one sentence:',
+            ],
+        ],
+        'max_tokens' => 50,
+    ]));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        'Authorization: Bearer ' . OPENAI_API_KEY
+    ]);
+
+    $response = curl_exec($ch);
+    if (!$response || curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 200) {
+        echo '<pre>';var_dump($response);echo '</pre>';
+        // Use the backup list if API fails
+        return $backup_list[array_rand($backup_list)];
+    }
+
+    $data = json_decode($response, true);
+    return $data['choices'][0]['message']['content'] ?? $backup_list[array_rand($backup_list)];
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $sentence = $_POST['sentence'];
+    // Validate sentence grammar using OpenAI API
+    $message = validate_sentence($sentence);
+}
+
+function validate_sentence($sentence) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, OPENAI_API_ENDPOINT);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+        'model' => getenv('OPENAI_CHATGPT_MODEL'),
+        'messages' => [
+            [
+                'role' => 'system',
+                'content' => 'You are Danish language teacher. Find grammatical errors in this sentence. Be accurate. Explain. Format as list.',
+            ],
+            [
+                'role' => 'user',
+                'content' => sprintf('"%s"', $sentence),
+            ],
+        ],
+        'max_tokens' => 400,
+    ]));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        'Authorization: Bearer ' . OPENAI_API_KEY
+    ]);
+
+    $response = curl_exec($ch);
+    if (!$response || curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 200) {
+        echo '<pre>';var_dump($response);echo '</pre>';
+        return false; // Fail silently if API fails
+    }
+
+    $data = json_decode($response, true);
+    return $data['choices'][0]['message']['content'];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Danish Sentence Generator</title>
+</head>
+<body>
+    <h1>Danish Sentence Generator</h1>
+    <p>Random Verb: <abbr title="<?= implode("\n", $random_verb)?>"><?= $random_verb[0]?></abbr></p>
+    <p>Random Conjunction: <abbr title="<?= implode("\n", $random_conjunction)?>"><?= $random_conjunction[0]?></abbr></p>
+    <p>Random Spongebob Situation: <strong><?= $random_situation?></strong></p>
+    <form action="" method="post">
+        <label for="sentence">Write a sentence using the above values:</label><br>
+        <textarea name="sentence" id="sentence" cols="50" rows="5"></textarea><br><br>
+        <input type="submit" value="Validate">
+    </form>
+    <?php if (isset($sentence)): ?>
+        <p>You entered: <b><?php echo $sentence; ?></b></p>
+    <?php endif; ?>
+    <?php if (isset($message)): ?>
+        <p>ChatGPT says:</p>
+        <pre><?php echo $message; ?></pre>
+        <p>(If in doubt, consult your teacher.)</p>
+    <?php endif; ?>
+    <style type="text/css">
+        abbr {
+            cursor: help;
+            font-weight: bold;
+        }
+        pre {
+            white-space: pre-wrap;       /* Since CSS 2.1 */
+            white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+            white-space: -pre-wrap;      /* Opera 4-6 */
+            white-space: -o-pre-wrap;    /* Opera 7 */
+            word-wrap: break-word;       /* Internet Explorer 5.5+ */
+        }
+    </style>
+</body>
+</html>
